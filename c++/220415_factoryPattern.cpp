e9238282651a4a4aed097ef47dda547ac2848106
CBInovator <cocobo2369@gmail.com>
2022-04-15 22:54:25 +0900

    220415_factoryPattern #3 생성하는 함수들을 별도의 클래스에 묶어 팩터리 사용

--- c++/220415_factoryPattern.cpp
+++ c++/220415_factoryPattern.cpp
@@ -3,12 +3,28 @@
 
 using namespace std;
 
-class Point{
+
+class Point
+{
     float x;
     float y;
+
+    //Point내에서 friend로 선언된 class는 protected, private 까지 접근이 가능하다!
+    //일종의 whitelist
+    friend class PointFactory;
+
 protected :
     Point(const float x, const float y) : x(x),y(y){}
 
+public:
+    void printPoint(){
+        cout << "x : " << x << endl;
+        cout << "y : " << y << endl;
+    }
+};
+
+class PointFactory
+{
 public:
     static Point Cartesian(float x, float y)
     {
@@ -19,19 +35,15 @@ public:
     {
         return { r*cos(theta), r*sin(theta) };
     }
-
-    void printPoint(){
-        cout << "x : " << x << endl;
-        cout << "y : " << y << endl;
-    }
 };
 
-int main(){
+int main()
+{
     //생성자가 드러나지 않음, 코드의 가독성도 좋음
-    Point p1 = Point::Cartesian(1.3,2.5);
+    Point p1 = PointFactory::Cartesian(1.3,2.5);
     p1.printPoint();
 
-    auto p2 = Point::Polar(2,0.5);
+    auto p2 = PointFactory::Polar(2,0.5);
     p2.printPoint();
 
     return 0;
